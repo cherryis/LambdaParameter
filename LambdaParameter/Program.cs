0617ade01180ea75a91cb35de55ffef8c149f71c
cherryis <59629395+cherryis@users.noreply.github.com>
2023-06-10 11:41:18 -0700

    Add project files.

---
+++ LambdaParameter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+namespace LambdaParameter
+{
+    class Program
+    {
+        //Declare the delegate
+        public delegate void Del(string s);
+
+        static void Main(string[] args)
+        {
+            //Just print something using the predefined method
+            Del handler = new Del(DelegateMethod1);  //Instantiate a named delegate
+            PrintIt("Hello world!", handler);
+
+            //Creating a function with a lambda
+            Write("\nEnter something to print: ");
+            Del reusableHandler = (inString) => WriteLine(inString);
+            reusableHandler(ReadLine());
+
+
+            //But you don't have to create anything.  You can just pass in the lambda exrpession!
+            Write("\nEnter something else to print: ");
+            PrintIt(ReadLine(), (inString) => WriteLine(inString));
+
+            ReadKey();
+        }
+
+        public static void PrintIt(string myString, Del Writer)
+        {
+            Writer(myString);
+        }
+        public static void DelegateMethod1(string message)
+        {
+            WriteLine(message);
+        }
+
+    }
+}
